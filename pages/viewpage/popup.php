e36f62585105d9b5441c3dd09574d7a5fc3cac0a
Bismay Kumar Mohapatra <bismay4u@gmail.com>
2017-11-20 16:31:47 +0530

    Added popup page to cms
    For forms, reports and infoviews

---
+++ pages/viewpage/popup.php
@@ -0,0 +1,16 @@
+<?php
+if(!defined('ROOT')) exit('No direct script access allowed');
+
+$slug=_slug();
+
+if(isset($slug["module"])) {
+	if(checkModule($slug["module"])) {
+// 		loadModule($slug["module"]);
+      _pageVar("MODULESRC",$slug["module"]);
+	} else {
+		trigger_logikserror("Sorry, Module '{$slug['module']}' not found.");
+	}
+} else {
+	trigger_logikserror("Sorry, Module not defined.");
+}
+?>
