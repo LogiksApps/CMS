c06a84fd250d3c4e0d0eb9b25cc49185b9e510cf
Bismay <bismay4u@gmail.com>
2018-02-07 14:52:22 +0530

    Updated Role Manager

--- plugins/modules/credsRoles/index.php
+++ plugins/modules/credsRoles/index.php
@@ -1,153 +1,57 @@
 <?php
 if(!defined('ROOT')) exit('No direct script access allowed');
 
-include_once __DIR__."/funcs.php";
-if(!isset($_GET['DX'])) {
-	return;
-}
-
 loadModule("pages");
 
-echo _css("credsRoles");
-
 function pageSidebar() {
-  $dataPrivilegesFinal=$_GET['DX'];
-  $dataRolesFinal=$_GET['DY'];
-  
-	// <form role='search'>
-	//     <div class='form-group'>
-	//       <input type='text' class='form-control' placeholder='Search'>
-	//     </div>
-	// </form>
   $html="<div id='componentTree' class='componentTree list-group list-group-root well'>";
   $html.="<ul class='list-group'>";
-  
-  $dx=0;
-  foreach($dataPrivilegesFinal as $k=>$p) {
-    if($p['id']<=ROLE_PRIME) {
-      continue;
-    }
-		$title=toTitle(_ling($p['name']));
-		$html.="<li class='list-group-item' title='Privilege : $title ({$p['id']})' ><a href='#{$p['hash']}' aria-controls='{$p['hash']}' role='tab' data-toggle='tab'>$title <i class='fa fa-chevron-right pull-right'></i></a></li>";
-//     if(isset($dataRolesFinal[$p['hash']])) {
-      
-//       if($dx==0) {
-// //         echo "<li role='presentation' title='Privilege : $title ({$p['id']})' class='active'><a href='#{$p['hash']}' aria-controls='{$p['hash']}' role='tab' data-toggle='tab'>$title</a></li>";
-//         $html.="<li class='list-group-item' title='Privilege : $title ({$p['id']})' class='active'><a href='#{$p['hash']}' aria-controls='{$p['hash']}' role='tab' data-toggle='tab'>$title <i class='fa fa-chevron-right pull-right'></i></a></li>";
-//       } else {
-// //         echo "<li role='presentation' title='Privilege : $title ({$p['id']})' ><a href='#{$p['hash']}' aria-controls='{$p['hash']}' role='tab' data-toggle='tab'>$title</a></li>";
-//         $html.="<li class='list-group-item' title='Privilege : $title ({$p['id']})' ><a href='#{$p['hash']}' aria-controls='{$p['hash']}' role='tab' data-toggle='tab'>$title <i class='fa fa-chevron-right pull-right'></i></a></li>";
-//       }
-
-//       $dx++;
-//     }
-  }
-  
+  $html.="<div class='ajaxloading ajaxloading5'></div>";
   $html.="</ul>";
   $html.="</div>";
 	return $html;
 }
 function pageContentArea() {
-  $dataPrivilegesFinal=$_GET['DX'];
-  $dataRolesFinal=$_GET['DY'];
-  
-  ob_start();
-?>
-<div class='col-xs-12'>
-<div id='roleTabs' class='row'>
-  <ul class="nav nav-tabs hidden" role="tablist">
-		<?php
-			$dx=0;
-			foreach($dataPrivilegesFinal as $k=>$p) {
-				if($p['id']<=ROLE_PRIME) {
-					continue;
-				}
-				//if(isset($dataRolesFinal[$p['hash']])) {
-					$title=toTitle(_ling($p['name']));
-					if($dx==0)
-						echo "<li role='presentation' title='Privilege : $title ({$p['id']})' class='active'><a href='#{$p['hash']}' aria-controls='{$p['hash']}' role='tab' data-toggle='tab'>$title</a></li>";
-					else
-						echo "<li role='presentation' title='Privilege : $title ({$p['id']})' ><a href='#{$p['hash']}' aria-controls='{$p['hash']}' role='tab' data-toggle='tab'>$title</a></li>";
-					
-					$dx++;
-				//}
-			}
-		?>
-<!-- 		<li class="pull-right"><input class="form-control" type="search" id="searchModule" placeholder='Search Module' ></li> -->
-  </ul>
-  <div id='roleTabModel' class="tab-content roleTabModel">
-		<?php
-			$dx=0;
-			foreach($dataPrivilegesFinal as $k=>$p) {
-				if($p['id']<=ROLE_PRIME) {
-					continue;
-				}
-				$titleHeader=toTitle(_ling($p['name']));
-				if($dx==0)
-					echo "<div role='tabpanel' class='tab-pane active' id='{$p['hash']}'>";
-				else
-					echo "<div role='tabpanel' class='tab-pane' id='{$p['hash']}'>";
-
-				echo "<div class='panel-group' role='tablist' aria-multiselectable='false' id='accordion{$p['hash']}'>";
-				echo "<div class='panel-heading panel-heading-bold'>$titleHeader</div>";
-
-				if(isset($dataRolesFinal[$p['hash']])) {
-					uksort($dataRolesFinal[$p['hash']],"roleSortModule");
-					
-					foreach($dataRolesFinal[$p['hash']] as $modName=>$modules) {
-						$modHash=md5($modName.$p['hash']);
-						echo "<div class='panel panel-default' data-module='".strtolower($modName)."' >";
-						echo "<div class='panel-heading' role='tab' id='{$modHash}'>";
-							echo "<h4 class='panel-title'>";
-								//echo "<input class='pull-left' type='checkbox' name='checkAll' />";
-								echo "<a class='accordion-toggle' role='button' data-toggle='collapse' data-parent='#accordion{$p['hash']}' href='#collapse{$modHash}' aria-expanded='true' aria-controls='collapse{$modHash}'>";
-								echo toTitle(_ling($modName));
-								echo "</a>";
-							echo "</h4>";
-						echo "</div>";
-						echo "<div id='collapse{$modHash}' class='panel-collapse collapse' role='tabpanel' aria-labelledby='{$modHash}'>";
-							echo "<div class='panel-body'>";
-								echo "<ul class='list-group'>";
-								foreach($modules as $role) {
-									$roleHash=md5($role['id'].$role['privilegehash']);
-									echo "<li class='list-group-item' guid='{$role['guid']}'><label>";
-									echo toTitle(_ling(strtolower($role['category']))." @ "._ling(strtolower($role['activity'])));
-									echo " <citie class='datalink' data-type='guid-users' data-value='{$role['guid']}'>[{$role['guid']}]</citie>";
-									if($role['allow']===true || $role['allow']=="true") {
-										echo "<input class='pull-right' type='checkbox' name='roleCheckbox' data-hash='{$roleHash}' checked />";// data-x='".json_encode($p)."'
-									} else {
-										echo "<input class='pull-right' type='checkbox' name='roleCheckbox' data-hash='{$roleHash}' />";
-									}
-									echo "</label></li>";
-								}
-								echo "</ul>";
-							echo "</div>";
-						echo "</div>";
-						echo "</div>";
-					}
-				} else {
-					echo "<h3 class='text-center'><br><br>Permissions Not Found for the site</h3>";
-				}
-				echo "</div>";
-				echo "</div>";
-				$dx++;
-			}
-		?>
-  </div>
-</div>
-</div>
-<?php
-  $htmlData=ob_get_contents();
-  ob_end_clean();
-	return "<div id='componentSpace' class='componentSpace'>$htmlData</div>";
+  return "<div id='componentSpace' class='componentSpace' style='padding-right: 10px;padding-left: 5px;'><div class='col-xs-12'><div class='row'>
+		<div id='roleModel' class='panel-group roleTabModel' role='tablist' aria-multiselectable='false'>
+			<div class='panel-heading panel-heading-bold'>Please Click On Left to Start</div>
+		</div>
+	</div></div></div>
+	
+	<script id='rolebox-template' type='text/x-handlebars-template'>
+		<div class='panel panel-default' data-module='{{modulehash}}'>
+			<div class='panel-heading' role='tab' id='{{modulehash}}'>
+				<h4 class='panel-title'>
+					<a class='accordion-toggle' role='button' data-toggle='collapse' data-parent='#roleModel' href='#collapse{{modulehash}}' aria-expanded='true' aria-controls='collapse{{modulehash}}'>{{module_title}}</a>
+				</h4>
+			</div>
+			<div id='collapse{{modulehash}}' class='panel-collapse collapse' role='tabpanel' aria-labelledby='{{modulehash}}'>
+				<div class='panel-body'>
+					<!--<ul class='list-group'></ul>-->
+				</div>
+			</div>
+		</div>
+	</script>
+	
+	<script id='roleitem-template' type='text/x-handlebars-template'>
+		<li class='list-group-item' guid='{{guid}}' data-module='{{module}}' data-category='{{category}}' data-activity='{{activity}}'  >
+			<label>
+				{{role_title}}
+				<citie class='datalink' data-type='guid-users' data-value='{{guid}}'>[{{guid}}]</citie>
+				{{#if allow}}
+					<input class='pull-right' type='checkbox' name='roleCheckbox' data-hash='{{rolehash}}' checked />
+				{{else}}
+					<input class='pull-right' type='checkbox' name='roleCheckbox' data-hash='{{rolehash}}' />
+				{{/if}}
+			</label>
+		</li>
+	</script>
+	";
 }
 
 $webPath=dirname(getWebPath(__FILE__))."/";
 
-loadModuleLib("cmsEditor","embed");
-
 echo _css("credsRoles");
-echo _js(["credsRoles"]);
 
 printPageComponent(false,[
 		"toolbar"=>[
@@ -156,8 +60,10 @@ printPageComponent(false,[
 			//"loadInfoComponent"=>["title"=>"About","align"=>"right"],
 			//"loadPreviewComponent"=>["title"=>"Preview","align"=>"right"],
 
-			["title"=>"Search Roles","type"=>"search","align"=>"right"]
-			//"listTemplates"=>["icon"=>"<i class='fa fa-refresh'></i>"],
+			["title"=>"Search Roles","type"=>"search","align"=>"right"],
+			
+			"reloadRoles"=>["icon"=>"<i class='fa fa-refresh'></i>"],
+			"generateRoles"=>["icon"=>"<i class='fa fa-gears'></i>","tips"=>"Generate New Roles"],
 			//"createTemplate"=>["icon"=>"<i class='fa fa-plus'></i>","tips"=>"Create New"],
 			//"openExternal"=>["icon"=>"<i class='fa fa-external-link'></i>","class"=>"onsidebarSelect"],
 			//"preview"=>["icon"=>"<i class='fa fa-eye'></i>","class"=>"onsidebarSelect onOnlyOneSelect","tips"=>"Preview Content"],
@@ -168,73 +74,6 @@ printPageComponent(false,[
 		"sidebar"=>"pageSidebar",
 		"contentArea"=>"pageContentArea"
 	]);
-
+echo "<script>FORSITE='{$_REQUEST["forsite"]}';</script>";
+echo _js(["credsRoles"]);
 ?>
-<script>
-FORSITE='{$_REQUEST["forsite"]}';
-var uniStatus=true;
-$(function() {
-  $("#componentTree .list-group-item:first-child").addClass("active");
-  $("#componentTree a").click(function() {
-      href=$(this).attr("href")
-      $("#roleTabs li>a[href='"+href+"']").tab("show");
-      return true;
-    });
-
-	$("input[name=checkAll]","#roleTabModel").each(function() {
-		if($(this).closest(".panel.panel-default").find("input[name=roleCheckbox]").length==$(this).closest(".panel.panel-default").find("input[name=roleCheckbox]:checked").length) {
-			this.checked=true;
-		}
-	});
-
-  $("#pgToolbarSearch").on('submit', function (e) {
-        e.preventDefault();
-        return false;
-    });
-  $("#pgToolbarSearch input").keyup(function(e) {
-    if(e.keyCode==13) return false;
-    vs=$("#pgToolbarSearch input").val();
-		if(vs==null || vs.length<=0) {
-			$(".tab-pane.active .panel").show();
-			return;
-		}
-		$(".tab-pane.active .panel:not([data-module^='"+vs+"'])").hide();
-		$(".tab-pane.active .panel[data-module^='"+vs+"']").show();
-	});
-	$("#roleTabModel").delegate("input[name=checkAll]","change",function(e) {
-			uniStatus=this.checked;
-			$(this).closest(".panel.panel-default").find("input[name=roleCheckbox]").each(function() {
-				roleHash=$(this).data("hash");
-				this.checked=uniStatus;
-				processAJAXPostQuery(_service("credsRoles","save"),roleHash+"="+uniStatus,function(ans) {
-					try {
-						json=$.parseJSON(ans);
-						if(json.Data=="success") {
-
-						} else {
-							lgksToast("Sorry, could not update the role.");
-						}
-					} catch($e) {
-						lgksToast("Sorry, could not update the role.");
-					}
-				});
-			});
-		});
-	$("#roleTabModel").delegate("input[name=roleCheckbox]","change",function(e) {
-		roleHash=$(this).data("hash");
-		status=$(this).is(":checked");
-		processAJAXPostQuery(_service("credsRoles","save"),roleHash+"="+status,function(ans) {
-			try {
-				json=$.parseJSON(ans);
-				if(json.Data=="success") {
-					lgksToast("Updated");
-				} else {
-					lgksToast("Sorry, could not update the role.");
-				}
-			} catch($e) {
-				lgksToast("Sorry, could not update the role.");
-			}
-		});
-	});
-});
-</script>
--- plugins/modules/credsRoles/service.php
+++ plugins/modules/credsRoles/service.php
@@ -3,6 +3,55 @@ if(!defined('ROOT')) exit('No direct script access allowed');
 checkServiceAccess();
 
 switch($_REQUEST["action"]) {
+	case "info":
+		$sql1=_db(true)->_selectQ(_dbTable("rolemodel",true),"module,count(*) as count")
+				->_whereOR("site",['*',CMS_SITENAME])
+				->_whereOR("guid",[$_SESSION['SESS_GUID'],'global'])->_GROUPBY("module")->_ORDERBY("module")->_GET();
+		$sql2=_db(true)->_selectQ(_dbTable("rolemodel",true),"module,activity,count(*) as count")
+				->_whereOR("site",['*',CMS_SITENAME])
+				->_whereOR("guid",[$_SESSION['SESS_GUID'],'global'])->_GROUPBY("concat(module,activity)")->_ORDERBY("module")->_GET();
+		$sql3=_db(true)->_selectQ(_dbTable("rolemodel",true),"module,activity,category,count(*) as count")
+				->_whereOR("site",['*',CMS_SITENAME])
+				->_whereOR("guid",[$_SESSION['SESS_GUID'],'global'])->_GROUPBY("concat(module,activity,category)")->_ORDERBY("category,module,activity")->_GET();
+		
+		printServiceMsg([$sql1,$sql2,$sq3]);
+		break;
+	case "list-privileges":
+		$sql=_db(true)->_selectQ(_dbTable("privileges",true),"id,site,name,blocked,remarks,md5(concat(id,name)) as privilegehash,md5(concat(id,name)) as hash")
+			//->_where(array("blocked"=>"false"))//,"length(hash)"=>[0,">"]
+			->_whereOR("site",['*',CMS_SITENAME])
+			->_whereOR("guid",[$_SESSION['SESS_GUID'],'global']);
+
+			$data=$sql->_GET();
+			foreach($data as $a=>$b) {
+				if($b['id']<=ROLE_PRIME) {
+					unset($data[$a]);
+					continue;
+				}
+				$data[$a]['title']=toTitle(_ling($b['name']));
+			}
+			printServiceMsg($data);
+		break;
+	case "list-roles":
+		if(isset($_POST['roleid'])) {
+			$sqlData=_db(true)->_selectQ(_dbTable("rolemodel",true),"id,guid,category,module,md5(concat(module)) as modulehash,activity,privilegehash,allow,role_type")
+            ->_where(array("privilegehash"=>$_POST['roleid']))
+						->_whereOR("site",['*',CMS_SITENAME]);
+			
+			$sqlData=$sqlData->_GET();
+			foreach($sqlData as $a=>$b) {
+				$sqlData[$a]['module_title']=toTitle(_ling($b['module']));
+				$sqlData[$a]['role_title']=toTitle(_ling(strtolower($b['category']))." @ "._ling(strtolower($b['activity'])));
+				
+				$sqlData[$a]['rolehash']=md5($b['id'].$b['privilegehash']);
+				$sqlData[$a]['allow']=($sqlData[$a]['allow']==="true");
+			}
+			
+			printServiceMsg($sqlData);
+		} else {
+			printServiceMsg([]);
+		}
+		break;
 	case "save":
 		$keys=array_keys($_POST);
 
@@ -24,5 +73,13 @@ switch($_REQUEST["action"]) {
 			printServiceMsg("error2");
 		}
 		break;
+	case "generate":
+		break;
+	case "generate-save":
+		break;
+}
+
+function roleSortModule($a,$b) {
+	return strcasecmp(strtolower($a), strtolower($b));
 }
 ?>
